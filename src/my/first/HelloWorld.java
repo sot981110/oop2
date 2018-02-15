06106c936149f64a41b673310b5e671e97b48c76
sot981110 <sot981110@gmail.com>
2018-02-15 15:32:30 +0100

    Test Commit

--- src/my/first/HelloWorld.java
+++ src/my/first/HelloWorld.java
@@ -3,7 +3,7 @@ package my.first;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		String msg = "Hello" + " " + "World";
+		String msg = "Hello" + "  " + "World";
 		System.out.println(msg);
 
 	}
